2f4a894a9ffedeb590dcf2e066a1174a55655a3c
palminski <williambolls98@gmail.com>
2022-07-31 18:51:16 -0700

    Can submit form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,14 @@
-let buttonEl = document.querySelector("#save-task");
+let formEl = document.querySelector("#task-form");
 let tasksToDoEl = document.querySelector("#tasks-to-do");
-console.log(buttonEl);
 
-let createTaskHandler = function() {
+
+let createTaskHandler = function(event) {
+    event.preventDefault();
+
     let listItemEl = document.createElement("li");
     listItemEl.textContent = "Added a new task!";
     listItemEl.className = "task-item";
     tasksToDoEl.appendChild(listItemEl);
 }
 
-buttonEl.addEventListener("click",createTaskHandler);
+formEl.addEventListener("submit",createTaskHandler);
