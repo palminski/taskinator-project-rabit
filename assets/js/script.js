f3594613691a73f96863d84aaf67031081574259
palminski <williambolls98@gmail.com>
2022-08-02 11:11:24 -0700

    Reloads on page!

--- assets/js/script.js
+++ assets/js/script.js
@@ -185,6 +185,54 @@ var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
+let loadTasks = function(){
+
+    tasks = localStorage.getItem("tasks");
+
+    if (tasks === null){
+        tasks = [];
+        return false;
+    }
+    tasks = JSON.parse(tasks);
+
+
+    //iterate thu array and create objects
+    for (let i =0; i <tasks.length;i++) {
+        
+        tasks[i].id = taskIdCounter;
+
+        let listItemEl = document.createElement("li");
+        listItemEl.className = "task-item";
+        listItemEl.setAttribute("data-task-id",tasks[i].id)
+        
+
+        let taskInfoEl = document.createElement("div");
+        taskInfoEl.className = "task-info";
+        taskInfoEl.innerHTML = "<h3 class='task-name'>"+tasks[i].name+"</h3><span class='task-type'>"+tasks[i].type+"</span>";
+
+        listItemEl.appendChild(taskInfoEl);
+        
+        let taskActionsEl = createTaskActions(tasks[i].id);
+        listItemEl.appendChild(taskActionsEl);
+        console.log(listItemEl);
+
+        if (tasks[i].status === "to do"){
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 0;
+            tasksToDoEl.appendChild(listItemEl);
+        }
+        else if (tasks[i].status === "in progress"){
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 1;
+            tasksInProgressEl.appendChild(listItemEl);
+        }
+        else if (tasks[i].status === "complete"){
+            listItemEl.querySelector("select[name='status-change']").selectedIndex = 2;
+            tasksCompletedEl.appendChild(listItemEl);
+        }
+        taskIdCounter ++;
+    }
+}
+
+loadTasks();
 formEl.addEventListener("submit",taskFormHandler);
 pageContentEl.addEventListener("click",taskButtonHandler);
 pageContentEl.addEventListener("change",taskStatusChangeHandler);
